6556a9b87c7d87b3fb5e0ac43249f1ed97f17126
Shivani Joshi <shivanis2194@gmail.com>
2020-06-24 11:41:26 -0400

    added tests for seachInput , searchButton and find restaurants

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,6 +1,8 @@
-Cypress.Commands.add('homePage',()=>{
-    cy.visit('/')
-    cy.title().should('eq', 'Order takeaway online from 30,000+ food delivery restaurants. | Just Eat')
-    //cy.url().should('eq', 'https://www.just-eat.co.uk/')
-
-})
+Cypress.Commands.add('visitHomePage', () => {
+  cy.visit('https://www.just-eat.co.uk/');
+  cy.title().should(
+    'eq',
+    'Order takeaway online from 30,000+ food delivery restaurants. | Just Eat'
+  );
+  cy.url().should('eq', 'https://www.just-eat.co.uk/');
+});
